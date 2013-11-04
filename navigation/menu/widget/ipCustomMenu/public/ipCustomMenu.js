7ecad466138841fc2a0c8e6c8c4184957ad4a76a
marijusimpresspages <marijus@impresspages.org>
2013-11-04 15:43:54 +0200

    Update ipCustomMenu.js

--- navigation/menu/widget/ipCustomMenu/public/ipCustomMenu.js
+++ navigation/menu/widget/ipCustomMenu/public/ipCustomMenu.js
@@ -1,44 +1,27 @@
 /**
  *
- * IpHelloInput widget javascript controller.
- * This file initializes widget management tools (if needed) and
- * prepare data to be stored on the database.
+ * ipCustomMenu widget javascript controller.
  *
  * @package ImpressPages
- * @copyright Copyright (C) 2011 ImpressPages LTD.
+ * @copyright Copyright (C) 2013 ImpressPages LTD.
  * @license GNU/GPL, see ip_license.html
  */
 
 function IpWidget_ipCustomMenu(widgetObject) {
-    /*
-     * widgetObject is a jQuery object of currently edited wiget
-     * you can use it to find elements inside it. Eg. widgetObject.find('.yourElementClass').val();
-     * Avoid genral selectors as for eg. 'input'. Because you can select some controlls of widgets instead of wat you want.
-     * Also you can use it to get your widget data from the database using function: this.widgetObject.data('ipWidget').data;
-     */
+
     this.widgetObject = widgetObject;
 
-
-    //just enable bellow described functions
     this.manageInit = manageInit;
     this.prepareData = prepareData;
 
 
     /*
      * Executed each time, when your widget goes into management state.
-     * Do whathever you need to initialize it:
-     * - bind events
-     * - populate default form values
-     * - initialize tinyMCE
-     * - ...
      */
     function manageInit() {
         //get widget data currently stored in the database
-//        var instanceData = this.widgetObject.data('ipWidget').data;
 
         var instanceData = this.widgetObject.data('ipWidget').data;
-
-        //if widget has been already initialized
 
         if (instanceData.menuDepth){
             this.widgetObject.find('.ipCustomMenuDepth').val(instanceData.menuDepth);
@@ -57,10 +40,9 @@ function IpWidget_ipCustomMenu(widgetObject) {
      *
      */
     function prepareData() {
-        //create simple data object. It will be returned as the data to be stored.
+
         var data = Object();
 
-        //set text value of an object
         data.menuDepth = parseInt(this.widgetObject.find('.ipCustomMenuDepth').val());
 
         if (this.widgetObject.find('.ipCustomMenuMode').prop("selectedIndex") == 0){
@@ -71,26 +53,6 @@ function IpWidget_ipCustomMenu(widgetObject) {
             data.zoneName = this.widgetObject.find('.ipCustomMenuMode').val();
         }
 
-        /* you can store as many data as you need.
-         * data.var1 = x;
-         * data.var2 = x;
-         * data.anything = x;
-         * ...
-         */
-
-        /*
-         * Return data object to ImpressPages system. Data that is returned
-         * will be stored to the database and will be available next time in manageInit function.
-         *
-         * Simple return statement will not work. You need to return value using event as it is shown bellow.
-         * This is done seeking flexability. Using this syntax you can
-         * initialize save process here, then wait for image uploads or other events
-         * and return result in different function where you will get final response
-         * about sucessfull save.
-         *
-         * If you don't know what that means, just use bellow example.
-         * You don't need to care too much.
-         */
         $(this.widgetObject).trigger('preparedWidgetData.ipWidget', [ data ]);
     }
 
